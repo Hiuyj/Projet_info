8b1b905c2eeb67fb7f775a09503ec36a6c4471da
Hiuyj <cyrillenouboue@gmail.com>
2016-02-05 10:50:43 +0100

    Create constantes.h

---
+++ constantes.h
@@ -0,0 +1,17 @@
+  #ifndef DEF_CONSTANTES
+    #define DEF_CONSTANTES
+
+        #define tle_blc 34  //taille d'un bloc en pixels
+        #define nb_blc_ht 12  // Nombre de blocs en hauteur
+        #define nb_blc_lg 12 // Nombre de blocs en largeur
+        #define lg_fnt (tle_blc*nb_blc_lg) // largeur de la fentre
+        #define ht_fnt (tle_blc*nb_blc_ht) // Hauteur de la fenetre
+
+        enum {VIDE, MUR, SOURIS};
+        enum states
+        {
+            STATE_MENU = 0,
+            STATE_GAME = 1,
+        };
+
+    #endif // DEF_CONSTANTES
