e001ae808796bfce04690983509a52bd72465fa8
Hiuyj <cyrillenouboue@gmail.com>
2016-02-05 10:54:13 +0100

    Create states.h

---
+++ states.h
@@ -0,0 +1,24 @@
+#ifndef STATES_H_INCLUDED
+#define STATES_H_INCLUDED
+
+#include "structures.h"
+
+// Fonction pour connaitre le state actuel
+int STATES_getState();
+
+// Fonction pour gérer les évènements en fonction du state actuel
+void STATES_handleEvent(SDL_Event event, Context* context);
+
+// Fonction pour mettre à jour en fonction du state actuel
+void STATES_update();
+
+// Fonction pour  dessiner en fonction du state actuel
+void STATES_render(SDL_Renderer* renderer);
+
+// Fonction pour aller d'un state à l'autre
+void STATES_switch(int newState, Context* context);
+
+
+
+
+#endif // STATES_H_INCLUDED
